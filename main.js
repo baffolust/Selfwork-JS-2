c4dcd1d844d5803f3c51f41609366eab5e2f0af2
Simone Mandorlini <djbaffo85@gmail.com>
2025-06-11 23:44:30 +0200

    Esercizio Compiuto

---
+++ main.js
@@ -0,0 +1,14 @@
+let num1 = 3;
+let num2 = 33;
+let num3 = 10;
+let num4 = 8;
+let num5 = 5;
+
+let somma;
+
+somma = num1+num2+num3+num4+num5;
+media = somma/5;
+
+console.log(`la somma dei 5 numeri è ${somma}`);
+console.log(`la media dei 5 numeri è ${media}`);
+
